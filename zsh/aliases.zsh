93337a8b3800f66d304cc531c249dff059614789
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2017-05-17 22:35:55 +0000

    Added forgiving aliases for cd-ing back

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,8 @@
 alias http='python -m SimpleHTTPServer'
 alias vi='vim'
 alias myip='curl icanhazip.com'
+alias cd..='cd ..'
+alias cd...='cd ...'
+alias cd....='cd ....'
+alias cd.....='cd .....'
 
